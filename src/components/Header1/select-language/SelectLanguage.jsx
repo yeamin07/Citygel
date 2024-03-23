3905c26cccd890dcc71de8c5d80eddfa60d7e7c3
Masud <masudrahman.dev@gmail.com>
2024-03-23 12:27:37 +0600

    test

--- src/components/Header1/select-language/SelectLanguage.jsx
+++ src/components/Header1/select-language/SelectLanguage.jsx
@@ -1,16 +1,14 @@
-import { Img, Text } from 'components';
-import React from 'react';
+import { Img, Text } from "components";
+import React from "react";
 
 const SelectLanguage = () => {
-    return (
-        <div className="flex text-white-A700 bg-black-900_87  p-3 rounded-lg  items-center gap-3">
+  return (
+    <div className="flex items-center gap-3 rounded-lg  bg-black-900_87 p-3  text-white-A700 text-red-200">
+      <div>icon |</div>
 
-            <div>icon |</div>
-            <div>Change Location</div>
-
-
-        </div>
-    );
+      <div>Change Location</div>
+    </div>
+  );
 };
 
 export default SelectLanguage;
