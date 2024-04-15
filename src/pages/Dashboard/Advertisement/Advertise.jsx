fc2ea8514bc4a96d53a1c0752477189ad51951b1
Demo-1 <hamedhasanalvi8@gmail.com>
2024-04-16 00:54:17 +0600

    uhfgjhf

--- src/pages/Dashboard/Advertisement/Advertise.jsx
+++ src/pages/Dashboard/Advertisement/Advertise.jsx
@@ -1,39 +1,61 @@
-import React from 'react'
+import { useState } from "react";
 import DefaultLayout from "components/Dashboard/layout/DefaultLayout";
-import { cetagory_logo } from 'assets/Allimages';
-import { nature } from 'assets/Allimages';
-import {
-  AdvertisingProvider,
-  AdvertisingSlot,
-  logVersionInfo
-} from 'react-advertising';
-import config from './config';
+import { cetagory_logo } from "assets/Allimages";
+import { nature } from "assets/Allimages";
+import { CopyBlock, dracula } from "react-code-blocks";
+import config from "./config";
 const Advertise = () => {
-  logVersionInfo();
+  // logVersionInfo();
+  // const [language, changeLanguage] = useState("jsx");
+  // const [languageDemo, changeDemo] = useState(sample["jsx"]);
+  // const [lineNumbers, toggleLineNumbers] = useState(true);
   return (
     <DefaultLayout classname="container mx-auto mb-6">
-       <div className='banner-parent-1'>
-         {/*~~~~~~~~~~~~~Each component~~~~~~~~~~~ */}
-          <div className='w-full border-[1px] border-solid border-gray-200 shadow-xl rounded-[15px] flex flex-col justify-start h-auto p-2'>
-          <h2 className='text-[20px] lg:text-[24px] h-[10%] w-full text-center'>Banner 1</h2>
-          <div className=' flex flex-col sm:flex-row justify-start gap-[4px] h-[90%] w-full'>
-          <div className='w-full sm:w-[48%]'>
-            <img src={nature} className='w-full h-[400px] object-cover rounded-lg' alt='Natural Beauty'/>
+      <div className="banner-parent-1">
+        {/*~~~~~~~~~~~~~Each component~~~~~~~~~~~ */}
+        <div className="flex h-auto w-full flex-col justify-start rounded-[15px] border-[1px] border-solid border-gray-200 p-2 shadow-xl">
+          <h2 className="h-[10%] w-full text-center text-[20px] lg:text-[24px]">
+            Banner 1
+          </h2>
+          <div className=" flex h-[90%] w-full flex-col justify-start gap-[4px] sm:flex-row">
+            <div className="w-full sm:w-[48%]">
+              <img
+                src={nature}
+                className="h-[400px] w-full rounded-lg object-cover"
+                alt="Natural Beauty"
+              />
+            </div>
+            <div className="flex w-full items-center justify-center font-serif text-[22px] font-semibold sm:w-[4%]">
+              Or
+            </div>
+            {/*~~~~~~~~~~~~~~~~~~Advertisement Section~~~~~~~~~~~~~ */}
+            <div className="w-full sm:w-[48%]">
+              <CopyBlock
+                language="go"
+                text={`class HelloMessage extends React.Component {
+                 handlePress = () => {
+                  alert('Hello')
+                   }
+             render() {
+                 return (
+                  <div>
+                  <p>Hello {this.props.name}</p>
+                    <button onClick={this.handlePress}>Say Hello</button>
+                  </div>
+                    );
+                  }
+                    }
+                  );`}
+                codeBlock
+                theme={dracula}
+                showLineNumbers={false}
+              />
+            </div>
           </div>
-          <div className='w-full sm:w-[4%] text-[22px] font-serif flex justify-center items-center font-semibold'>
-            Or 
-          </div>
-          <div className='w-full sm:w-[48%]'>
-          <AdvertisingProvider config={config} className="flex justify-evenly flex-row">
-          <h2 className='text-[25px] font-serif'>Google ad Section </h2>
-          <AdvertisingSlot id="banner-ad" />
-          </AdvertisingProvider>
-          </div>
-         </div>
-          </div>
-       </div>
+        </div>
+      </div>
     </DefaultLayout>
-  )
-}
+  );
+};
 
-export default Advertise
+export default Advertise;
